2488f8db37696e9a8e89b83980aea576c9544b17
sowmyayerramsetty1 <yerramsetty.sowmya0@gmail.com>
2025-09-08 19:22:44 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -46,3 +46,41 @@ module tt_um_sowmya_hamming_top (
 
 endmodule
 
+module encoder(
+    output [6:0] encoded_out,
+    input  [3:0] data_in
+);
+    assign encoded_out[6:4] = data_in[3:1]; 
+    assign encoded_out[2]   = data_in[0];   
+
+    assign encoded_out[0] = encoded_out[6] ^ encoded_out[4] ^ encoded_out[2]; 
+    assign encoded_out[1] = encoded_out[6] ^ encoded_out[5] ^ encoded_out[2]; 
+    assign encoded_out[3] = encoded_out[6] ^ encoded_out[5] ^ encoded_out[4]; 
+endmodule
+
+module decoder(
+    output reg [3:0] corrected_out,
+    output reg [3:0] error_out,
+    input  [6:0] encoded_in
+);
+    wire [2:0] error_pos;
+
+  
+    assign error_pos[0] = encoded_in[0] ^ encoded_in[6] ^ encoded_in[4] ^ encoded_in[2];
+    assign error_pos[1] = encoded_in[1] ^ encoded_in[6] ^ encoded_in[5] ^ encoded_in[2];
+    assign error_pos[2] = encoded_in[3] ^ encoded_in[6] ^ encoded_in[5] ^ encoded_in[4];
+
+    always @(*) begin
+
+        error_out = {encoded_in[6:4], encoded_in[2]};
+        corrected_out = error_out;
+
+        case (error_pos)
+            3'b011: corrected_out = {encoded_in[6:4], ~encoded_in[2]};
+            3'b101: corrected_out = {encoded_in[6:5], ~encoded_in[4], encoded_in[2]};
+            3'b110: corrected_out = {encoded_in[6], ~encoded_in[5], encoded_in[4], encoded_in[2]};
+            3'b111: corrected_out = {~encoded_in[6], encoded_in[5:4], encoded_in[2]};
+        endcase
+    end
+endmodule
+
